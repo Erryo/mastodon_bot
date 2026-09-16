36b16fc4dc394a4486a7c5a8695c9197e58d0d55
Erryo <trifanm2407@gmail.com>
2026-09-16 15:42:14 +0200

    fix eror

--- Poster.py
+++ Poster.py
@@ -53,6 +53,7 @@ class Poster:
             await self.db_q.put(
                 DBRequest(RequestType.POST_IGNORED, content=(status, e.reason))
             )
+            return
 
         if len(gen_post) >= 500:
             print("exceeded len")
@@ -68,7 +69,7 @@ class Poster:
             await self.db_q.put(
                 DBRequest(
                     RequestType.POST_PUBLISHED,
-                    (post.id, status.id, gen_post, int(end - start)),
+                    (post, status.id, gen_post, int(end - start)),
                 )
             )
         except Exception as e:
